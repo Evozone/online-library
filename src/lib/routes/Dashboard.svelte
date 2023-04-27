6a421ef0b8b5c46a9d9ec16967e28a076793d887
vishal-codes <itsvishal2417@gmail.com>
2023-04-27 20:36:37 +0530

    feat: add search book
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

--- src/lib/routes/Dashboard.svelte
+++ src/lib/routes/Dashboard.svelte
@@ -1,106 +1,129 @@
 <script>
-    // Imports
-    import { onMount } from "svelte";
-    import { db } from "../../firebase";
-    import { collection, getDocs, query } from "firebase/firestore";
+  // Imports
+  import { onMount } from "svelte";
+  import { db } from "../../firebase";
+  import { collection, getDocs, query, where } from "firebase/firestore";
 
-    // Stores
-    import { showLoading } from "../../stores";
+  // Stores
+  import { showLoading } from "../../stores";
 
-    // Components
-    import Navbar from "../components/Navbar.svelte";
-    import UploadBookBtn from "../components/UploadBookBtn.svelte";
-    import BookCard from "../components/BookCard.svelte";
+  // Components
+  import Navbar from "../components/Navbar.svelte";
+  import UploadBookBtn from "../components/UploadBookBtn.svelte";
+  import BookCard from "../components/BookCard.svelte";
 
-    export let id;
-    export let location;
+  export let id;
+  export let location;
 
-    let bookList = [];
-    let searchTerms = "";
+  let bookList = [];
+  let searchTerms = "";
 
-    // Fetch all books from Firestore
-    const getBooks = async () => {
-        $showLoading = true;
-        try {
-            const booksRef = await getDocs(query(collection(db, "books")));
-            const data = booksRef.docs.map((doc) => doc.data());
-            bookList = data;
-            $showLoading = false;
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  // Fetch all books from Firestore
+  const getBooks = async () => {
+    $showLoading = true;
+    try {
+      const booksRef = await getDocs(query(collection(db, "books")));
+      const data = booksRef.docs.map((doc) => doc.data());
+      bookList = data;
+      $showLoading = false;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const updateSearchTerms = (event) => {
-        // Set timeout
-        setTimeout(() => {
-            searchTerms = event.target.value;
-            console.log(searchTerms);
-        }, 1000);
-    };
+  const searchBoook = async () => {
+    $showLoading = true;
+    const q = query(
+      collection(db, "books"),
+      where("tags", "array-contains", searchTerms.toLowerCase())
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      if (querySnapshot.empty) {
+        alert("No matching documents.");
+      } else {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+        });
+      }
+    } catch (error) {
+      alert("Error getting documents ");
+      console.log(error);
+    } finally {
+      $showLoading = false;
+    }
+  };
 
-    onMount(async () => {
-        await getBooks();
-    });
+  onMount(async () => {
+    await getBooks();
+  });
 </script>
 
 <!-- Navbar -->
 <Navbar />
 
 <main>
-    <!-- Upload book button -->
-    <div class="container-fluid">
-        <h1 class="hero-title">List of Books</h1>
-        <div class="search-bar">
-            <input class="search" type="search" id="search" name="search" placeholder="Search" />
-            <UploadBookBtn />
-        </div>
+  <!-- Upload book button -->
+  <div class="container-fluid">
+    <h1 class="hero-title">List of Books</h1>
+    <div class="search-bar">
+      <input
+        on:change={searchBoook}
+        class="search"
+        type="search"
+        id="search"
+        name="search"
+        placeholder="Search"
+        bind:value={searchTerms}
+      />
+      <UploadBookBtn />
     </div>
+  </div>
 
-    <!-- List of books in a card -->
-    <article class="books">
-        {#await bookList}
-            <p>Loading...</p>
-        {:then books}
-            {#if books.length === 0}
-                <p>No books found</p>
-            {:else}
-                {#each books as book}
-                    <BookCard {book} />
-                {/each}
-            {/if}
-        {/await}
-    </article>
+  <!-- List of books in a card -->
+  <article class="books">
+    {#await bookList}
+      <p>Loading...</p>
+    {:then books}
+      {#if books.length === 0}
+        <p>No books found</p>
+      {:else}
+        {#each books as book}
+          <BookCard {book} />
+        {/each}
+      {/if}
+    {/await}
+  </article>
 </main>
 
 <style>
-    .books {
-        display: flex;
-        flex-direction: column;
-    }
+  .books {
+    display: flex;
+    flex-direction: column;
+  }
 
-    main {
-        padding: 2rem;
-        display: flex;
-        flex-direction: column;
-    }
+  main {
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+  }
 
-    .container-fluid {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
+  .container-fluid {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-    .search-bar {
-        display: flex;
-        align-items: center;
-    }
+  .search-bar {
+    display: flex;
+    align-items: center;
+  }
 
-    .search {
-        background-color: white;
-        padding: 0.5rem;
-        height: 2rem;
-        margin-bottom: 0;
-        margin-right: 1rem;
-    }
+  .search {
+    background-color: white;
+    padding: 0.5rem;
+    height: 2rem;
+    margin-bottom: 0;
+    margin-right: 1rem;
+  }
 </style>
